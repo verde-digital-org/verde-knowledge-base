be9007feff6d7c975bcb1bd2f6b7211ceba7c432
_shanith <mail.shanith@gmail.com>
2023-11-25 00:49:24 +0530

    update subdomain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://knowledge.verdedigital.io/',
+  url: 'https://docs.verdedigital.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
