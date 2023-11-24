31de593970bcd7a98bb0a8c7c6d360a24d325df3
_shanith <mail.shanith@gmail.com>
2023-11-24 21:08:42 +0530

    update docusaurus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,20 +3,22 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Verde Digital Knowledge Base',
+  tagline: 'Simplify your Sustainability Disclosure Reporting',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://verde-digital-org.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/verde-knowledge-base/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'verde-digital-org', // Usually your GitHub org/user name.
+  projectName: 'verde-knowledge-base', // Usually your repo name.
+  deploymentBranch: 'gh-pages', // The branch of your docs repo that you are publishing to GitHub pages
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
