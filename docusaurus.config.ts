3a1139a07ea60014d6a53c4f589a6ee4e4fde336
_shanith <mail.shanith@gmail.com>
2023-11-24 23:53:34 +0530

    update prod urls

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://verde-digital-org.github.io/',
+  url: 'https://knowledge.verdedigital.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/verde-knowledge-base/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
